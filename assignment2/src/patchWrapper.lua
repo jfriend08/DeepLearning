85da2ebd242899ba689399beb72fc1ecc2f0e451
jfriend08 <petersungjfriend08@gmail.com>
2016-03-09 13:51:57 +0000

    new centroid

--- assignment2/src/patchWrapper.lua
+++ assignment2/src/patchWrapper.lua
@@ -2,7 +2,7 @@ dofile './patchRunII.lua'
 
 patchRun = patchRun(40000)
 patchRun:normalize()
-patchRun:getPatch(3, 2, 50, true) -- kSize, gap, nPatch, randomPatch
+patchRun:getPatch(3, 2, 10, true) -- kSize, gap, nPatch, randomPatch
 patchRun:whiten()
 patchRun:runKmean(64, 1000) --ncentroids, niter
 torch.save('patchProvider_3_40000_1000_64c.t7', patchRun)
