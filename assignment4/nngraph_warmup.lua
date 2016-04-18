a7963c80689dd1de8325241b83a8b3c86074591d
jfriend08 <ys486@cornell.edu>
2016-04-18 12:01:03 -0400

    doc

--- assignment4/nngraph_warmup.lua
+++ assignment4/nngraph_warmup.lua
@@ -1,19 +1,22 @@
 nn = require 'nn'
 require 'nngraph'
 
-
+--First part for tanh
 h1     = nn.Linear(4,2)()
 h1_add = nn.Add(1)(h1)
 hh1    = nn.Tanh()(h1_add)
 hhh1   = nn.Square()(hh1)
 
+--Second part for sigmold
 h2     = nn.Linear(5,2)()
 h2_add = nn.Add(1)(h2)
 hh2    = nn.Sigmoid()(h2_add)
 hhh2   = nn.Square()(hh2)
 
+--Third part for adding input z
 h3     = nn.Identity()()
 
+--Combinding graph
 mmul   = nn.CMulTable()({hhh1, hhh2})
 madd   = nn.CAddTable()({mmul, h3})
 
